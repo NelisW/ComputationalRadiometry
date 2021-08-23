f26ae45b46f029c91a435caa67c98bad0d424ff3
NelisW <neliswillers@gmail.com>
2021-08-23 21:02:34 +0200

    updates to fix after anaconda update

--- _cleanAllTeX.py
+++ _cleanAllTeX.py
@@ -3,29 +3,66 @@
 
 import os.path, fnmatch
 
-#lists the files in a directory and subdirectories (from Python Cookbook)
-def listFiles(root, patterns='*', recurse=1, return_folders=0):
+
+################################################################
+#lists the files in a directory and subdirectories
+#this code is adapted from a recipe in the Python Cookbook
+def listFiles(root, patterns='*', recurse=1, return_folders=0, useRegex=False):
+    """Lists the files/directories meeting specific requirement
+
+        Returns a list of file paths to files in a file system, searching a 
+        directory structure along the specified path, looking for files 
+        that matches the glob pattern. If specified, the search will continue 
+        into sub-directories.  A list of matching names is returned. The 
+        function supports a local or network reachable filesystem, but not URLs.
+
+    Args:
+        | root (string): directory root from where the search must take place
+        | patterns (string): glob/regex pattern for filename matching. Multiple pattens 
+          may be present, each one separated by ;
+        | recurse (unt): flag to indicate if subdirectories must also be searched (optional)
+        | return_folders (int): flag to indicate if folder names must also be returned (optional)
+        | useRegex (bool): flag to indicate if patterns areregular expression strings (optional)
+
+    Returns:
+        | A list with matching file/directory names
+
+    Raises:
+        | No exception is raised.
+    """
+    if useRegex:
+        import re
+
     # Expand patterns from semicolon-separated string to list
     pattern_list = patterns.split(';')
-    # Collect input and output arguments into one bunch
-    class Bunch:
-        def __init__(self, **kwds): self.__dict__.update(kwds)
-    arg = Bunch(recurse=recurse, pattern_list=pattern_list,
-        return_folders=return_folders, results=[])
+    filenames = []
+    filertn = []
 
-    def visit(arg, dirname, files):
-        # Append to arg.results all relevant files (and perhaps folders)
-        for name in files:
-            fullname = os.path.normpath(os.path.join(dirname, name))
-            if arg.return_folders or os.path.isfile(fullname):
-                for pattern in arg.pattern_list:
-                    if fnmatch.fnmatch(name, pattern):
-                        arg.results.append(fullname)
+    for dirpath,dirnames,files in os.walk(root):
+        if dirpath==root or recurse:
+            for filen in files:
+                # filenames.append(os.path.abspath(os.path.join(os.getcwd(),dirpath,filen)))
+                filenames.append(os.path.relpath(os.path.join(dirpath,filen)))
+            if return_folders:
+                for dirn in dirnames:
+                    # filenames.append(os.path.abspath(os.path.join(os.getcwd(),dirpath,dirn)))
+                    filenames.append(os.path.relpath(os.path.join(dirpath,dirn)))
+
+    for name in filenames:
+        if return_folders or os.path.isfile(name):
+            for pattern in pattern_list:
+                if useRegex:
+                    #search returns None is pattern not found
+                    regex = re.compile(pattern)
+                    if regex.search(name):
+                        filertn.append(name)
                         break
-        # Block recursion if recursion was disallowed
-        if not arg.recurse: files[:]=[]
-    os.path.walk(root, visit, arg)
-    return arg.results
+                else:
+                    # split only the filename to compare, discard folder path
+                    if fnmatch.fnmatch(os.path.basename(name), pattern):
+                        filertn.append(name)
+                        break
+    return filertn
 
 #function to delete files in specified directory
 #  first parameter defines if the search must be recursively 0=not, 1=recursive
@@ -36,8 +73,8 @@ def QueryDelete(recurse,dir,patn):
     thefiles = listFiles(dir, patn,recurse)
     if len(thefiles)>0:
         for filename in thefiles:
-            print filename
-        instr=raw_input("Delete these files? (y/n)")
+            print(filename)
+        instr=input("Delete these files? (y/n)")
         if instr=='y':
             for filename in thefiles:
                 os.remove(filename)
